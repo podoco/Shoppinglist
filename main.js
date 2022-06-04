98ba0a4e3158524a78bf5dfa35159f182a0f6b8a
podoco <mingijeon83@gmail.com>
2022-06-04 12:12:30 +0900

    css

--- main.js
+++ main.js
@@ -20,35 +20,20 @@ function onAdd(){
     input.focus();
 }
 
+let id = 0;  //UUID
 function createItem(text) {
-
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class','item__row');
-
-    const item = document.createElement('div');
-    item.setAttribute('class','item');
-
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item_name');
-    name.innerText = text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class','item_delete');
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
-    deleteBtn.addEventListener('click',()=>{
-        items.removeChild(itemRow);
-    });
-
-    const itemDivider =document.createElement('div');
-    itemDivider.setAttribute('class', 'item_divider');
-
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    itemRow.innerHTML = `
+            <div class="item" data-id =${id}>
+                    <span class="item_name">${text}</span>
+                    <button class="item_delete" data-id =${id}>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+            </div>
+            <div class="item_divider"></div>`;
+    id++;
     return itemRow;
-
 }
 
 addBtn.addEventListener('click', () => {
@@ -60,5 +45,12 @@ input.addEventListener('keypress', (event) => {
     if(event.key ==='Enter'){
         onAdd();
     }
+});
+
+items.addEventListener('click',event => {
+    if(event.target.nodeName === 'I'){
+        console.log('he');
+    }
+
 });
 
