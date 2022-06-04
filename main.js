4a9b52b941064858e01c4852fc0950ab18644259
podoco <mingijeon83@gmail.com>
2022-06-04 13:40:20 +0900

    js2

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
 const input = document.querySelector('.footer_input');
 const addBtn = document.querySelector('.footer_button');
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    onAdd();
+})
+
 
 function onAdd(){
     //1.사용자가 입력한 텍스트를 받아옴
@@ -21,14 +28,16 @@ function onAdd(){
 }
 
 let id = 0;  //UUID
+
 function createItem(text) {
     const itemRow = document.createElement('li');
-    itemRow.setAttribute('class','item__row');
+    itemRow.setAttribute('class','item_row');
+    itemRow.setAttribute('data-id', id);
     itemRow.innerHTML = `
-            <div class="item" data-id =${id}>
+            <div class="item">
                     <span class="item_name">${text}</span>
-                    <button class="item_delete" data-id =${id}>
-                        <i class="fa-solid fa-trash-can"></i>
+                    <button class="item_delete">
+                        <i class="fa-solid fa-trash-can"  data-id =${id}></i>
                     </button>
             </div>
             <div class="item_divider"></div>`;
@@ -41,15 +50,19 @@ addBtn.addEventListener('click', () => {
 });
 
 
-input.addEventListener('keypress', (event) => {
-    if(event.key ==='Enter'){
-        onAdd();
-    }
-});
+
+// input.addEventListener('keypress', (event) => {
+//     if(event.key ==='Enter'){
+//         onAdd();
+//     }
+// });
 
 items.addEventListener('click',event => {
-    if(event.target.nodeName === 'I'){
-        console.log('he');
+    const id = event.target.dataset.id;
+
+    if (id) {
+        const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+        toBeDeleted.remove();        
     }
 
 });
